d29b8abaa52578bddac80bd6ecdf2b1ad19bad2c
Brian Dwyer <bdwyer@IEEE.org>
2018-10-22 11:26:16 -0400

    Add some initial retry logic
    Signed-off-by: Brian Dwyer <bdwyer@IEEE.org>

--- builtin/provisioners/chef/resource_provisioner.go
+++ builtin/provisioners/chef/resource_provisioner.go
@@ -708,8 +708,10 @@ func (p *provisioner) runCommand(o terraform.UIOutput, comm communicator.Communi
 		switch cmd.ExitStatus {
 		case 35:
 			o.Output("Reboot has been scheduled in the run state")
+			p.runCommand(o, comm, command)
 		case 37:
 			o.Output("Reboot needs to be completed")
+			p.runCommand(o, comm, command)
 		default:
 			return err
 		}
